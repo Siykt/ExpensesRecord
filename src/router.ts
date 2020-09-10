e1cc177fb12e1b53f9b70e19c2e4b0726a4fefc9
siykt <cnsiykt@163.com>
2020-09-10 11:01:51 +0800

    Update: 更新title为config

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
+import Config from './config';
 
 const router = Router();
 
 router.get(/(index)?/, (req, res, next) => {
-  res.render('index.ejs', { title: 'test' });
+  res.render('index.ejs', { title: Config.title });
 });
 
 export default router;
